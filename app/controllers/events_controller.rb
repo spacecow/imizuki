a7d2e95b393f3f6f7d99e65d190ccebaa90bf34a
Johan Sveholm <jsveholm@gmail.com>
2011-10-11 17:13:45 +0900

    testing iphone login

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,14 @@
 class EventsController < ApplicationController
-  http_basic_authenticate_with :name => "frodo", :password => "thering", :only => :destroy
   load_and_authorize_resource
+  #http_basic_authenticate_with :name => current_user.username, :password => "thering", :only => :destroy
+  before_filter :authenticate, :only => :destroy
 
   def show
   end
 
   def index
+    p current_user 
+    p "--------------------------"
     respond_to do |f|
       f.html
       f.json {render :json => @events}
@@ -34,4 +37,12 @@ class EventsController < ApplicationController
     @event.destroy
     redirect_to events_path
   end
+
+  private
+
+    def authenticate
+      authenticate_or_request_with_http_basic do |username,password|
+        @user = User.authenticate(username,password)
+      end
+    end
 end
