c8105493e29eeb2882436a125b5009993c46f3a3
Johan Sveholm <jsveholm@gmail.com>
2011-10-17 09:58:14 +0900

    fixed http basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
       unless current_user || Rails.env.test?
         authenticate_or_request_with_http_basic do |username,password|
           @user = User.authenticate(username,password)
-          session[:user_id] = @user.id
+          session[:user_id] = @user.id if @user
         end
       end
     end
